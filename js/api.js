9462d91cd8250f46b171b929d12b65568569fd6f
Papawarin <review3848@gmail.com>
2025-12-18 11:29:59 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 export const API_BASE =
-  "https://script.google.com/macros/s/AKfycbyNgj59KghdQsxwBN22utiZl7EVJONG21YZ4saKnFJN1I1MQc2cHDw7pmq7paaArlTy2g/exec";
+  "https://script.google.com/macros/s/AKfycbwv3l2UIDdo7WHeYd9NonQz9fxQN4puZyY2RbrEi8cPPdtS12B0uo2Ay6_ivpg36CoNXw/exec";
 
 export async function callApi(action, payload = {}) {
   const res = await fetch(API_BASE, {   // ✅ ใช้ให้ตรงชื่อ
