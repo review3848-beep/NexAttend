ea0f38535543f4999087c1b4074d7c65d13f7aa1
Papawarin <review3848@gmail.com>
2025-12-18 10:22:44 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,8 +1,8 @@
 export const API_BASE =
-  "https://script.google.com/macros/s/AKfycbx7En0A3MoqZHDsdWSvSoca73VdEKZxwIiPr3bJUsZYi47WRG2HPKfQCTXD8WQy-9bgDQ/exec";
+  "https://script.google.com/macros/s/AKfycbyNgj59KghdQsxwBN22utiZl7EVJONG21YZ4saKnFJN1I1MQc2cHDw7pmq7paaArlTy2g/exec";
 
 export async function callApi(action, payload = {}) {
-  const res = await fetch(API_BASE, {
+  const res = await fetch(API_BASE, {   // ✅ ใช้ตัวแปรให้ตรง
     method: "POST",
     headers: {
       "Content-Type": "text/plain;charset=utf-8"
@@ -13,10 +13,5 @@ export async function callApi(action, payload = {}) {
     })
   });
 
-  if (!res.ok) {
-    throw new Error("Network error");
-  }
-
   return await res.json();
 }
-
